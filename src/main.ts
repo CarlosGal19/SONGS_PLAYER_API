9eef4972e294955b650d18004b85d09182e859aa
Carlos Galindo <c.galindo.islas@gmail.com>
2026-06-21 14:18:26 -0600

    feat: create global pipe for validate requests

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
+  app.setGlobalPrefix('v1');
 
   if (process.env.NODE_ENV === 'development') {
     const config = new DocumentBuilder()
